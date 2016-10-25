60a98854d0efbb2a3efeff7060b29e0406541a7c
Yoni Kilzi <yoni.kilzi@gmail.com>
2016-10-25 11:30:44 +0300

    New folder structure

--- arcade-game/js/app.js
+++ arcade-game/js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(speed, row) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -101;
+    this.y = 60 + 83 * (row || Math.floor(Math.random() * 10) % 3);
+    this.speed = speed || 1;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // console.log(`Enemy: (${Math.floor(this.x)},${this.y})`);
+    this.x %= 505;
+    this.x += 101 * dt * this.speed;
+    if (this.x >= 505) {
+        this.x = -101;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -22,13 +31,53 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 101 * 2;
+    this.y = 57 * 7;
+};
+
+// This class requires an update(),
+Player.prototype.update = function() {
+    // TODO: Implement this method!
+};
+
+// render() and
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // a handleInput() method.
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'left':
+            this.x -= (this.x <= 0) ? 0 : 101; 
+            break;
+        case 'right':
+            this.x += (this.x >= 404) ? 0 : 101;
+            break;
+        case 'up':
+            this.y -= (this.y <= -16) ? 0 : 83;
+            break;
+        case 'down':
+            this.y += (this.y >= 399) ? 0 : 83;
+            break;
+    }
+    console.log(`Player: (${this.x},${this.y})`);
+};
 
 
 // Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+[0, 1, 2].forEach(function(assignedRow) {
+    var randomSpeed = Math.floor(Math.random() * 10) % 3 + 1;
+    allEnemies.push(new Enemy(randomSpeed, assignedRow));
+});
+
 // Place the player object in a variable called player
+var player = new Player();
 
 
 
